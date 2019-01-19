195190dcbe8bed12c27406aa495788ae8e53d210
Caden Albaugh <cadenalbaugh@gmail.com>
2019-01-19 11:43:03 -0600

    feat: add url parsing logic to normalize the requested path

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const { pathname } = parsedUrl;
+  const trimmedPath = pathname.replace(/^\/+|\/+$/g, "");
+
   res.end("Hello World\n");
+
+  console.log(`Request recieved on path ${trimmedPath}`);
 });
 
 server.listen(3000, () => {
