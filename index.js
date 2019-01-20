56b723db4d0a52a40cf65645ca299a198992e02f
Caden Albaugh <cadenalbaugh@gmail.com>
2019-01-20 11:07:12 -0600

    feat: wire up the config environment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
+const config = require("./config");
 
 const server = http.createServer((req, res) => {
   const { headers, url: requestUrl } = req;
@@ -50,8 +51,8 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("The server is listening on port 3000 now");
+server.listen(config.port, () => {
+  console.log(`The server is listening on port ${config.port} now`);
 });
 
 const handlers = {};
