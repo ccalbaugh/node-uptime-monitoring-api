3f0f06379c926f5606249b8b026db4af0e96a944
Caden Albaugh <cadenalbaugh@gmail.com>
2019-01-20 11:05:06 -0600

    feat: configure environment to export

--- config.js
+++ config.js
@@ -9,3 +9,15 @@ environments.production = {
   port: 5000,
   envName: "production"
 };
+
+const currentEnvironment =
+  typeof process.env.NODE_ENV == "string"
+    ? process.env.NODE_ENV.toLowerCase()
+    : "";
+
+const environmentToExport =
+  typeof environments[currentEnvironment] == "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
+
+module.exports = environmentToExport;
