d54eab881fb666a0bf3f61a07788a55c9bc82c6b
Caden Albaugh <cadenalbaugh@gmail.com>
2019-01-20 22:48:42 -0600

    feat: create separate implementation for https server instantiation'

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+const fs = require("fs");
 const http = require("http");
+const https = require("https");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 const config = require("./config");
 
-const server = http.createServer((req, res) => {
+const httpServer = http.createServer((req, res) => {
   unifiedServer(req, res);
 });
 
-server.listen(config.port, () => {
-  console.log(
-    `The server is listening on port ${config.port} in ${config.envName} mode`
-  );
+httpServer.listen(config.httpPort, () => {
+  console.log(`The server is listening on port ${config.httpPort}`);
+});
+
+const httpsServerOptions = {
+  key: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem")
+};
+
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+  unifiedServer(req, res);
+});
+
+httpsServer.listen(config.httpsPort, () => {
+  console.log(`The server is listening on port ${config.httpsPort}`);
 });
 
 const unifiedServer = (req, res) => {
