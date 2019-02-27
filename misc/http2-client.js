80c0611cca222394567b96a2340eaf3a1fbc51e3
Caden Albaugh <cadenalbaugh@gmail.com>
2019-02-27 14:10:07 -0600

    feat: create misc folder for experimental code and add http2 example

---
+++ misc/http2-client.js
@@ -0,0 +1,20 @@
+const http2 = require("http2");
+
+const client = http2.connect("http://localhost:6000");
+
+const req = client.request({
+  ":path": "/"
+});
+
+let str = "";
+
+req.on("data", chunk => {
+  console.log(chunk);
+  str += chunk;
+});
+
+req.on("end", () => {
+  console.log(str);
+});
+
+req.end();
---
+++ misc/http2-server.js
@@ -0,0 +1,13 @@
+const http2 = require("http2");
+
+const server = http2.createServer();
+
+server.on("stream", (stream, headers) => {
+  stream.respond({
+    status: 200,
+    "content-type": "text/html"
+  });
+  stream.end("<html><body><p>Hello World</p></body></html>");
+});
+
+server.listen(6000);
